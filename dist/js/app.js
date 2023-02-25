4b64b0b915c6006ff2408fa6a3409b5766bbf16e
lucas arcanjo <lucas.saolliveira@gmail.com>
2023-02-24 22:18:34 -0300

    change js to ts

--- dist/js/app.js
+++
@@ -1,6 +0,0 @@
-import { Negociacao } from "./models/negociacao.js";
-
-const negociacao = new Negociacao(new Date(), 10, 100);
-console.log(negociacao)
-negociacao.quantidade = 11 
-console.log(negociacao)
